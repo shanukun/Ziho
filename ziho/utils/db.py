8903ebcbfb349177302205a49c1f85087fccc3e0
Pratham <prathms007@gmail.com>
2024-03-08 18:42:00 +0530

    utils: Handle database error.

--- ziho/utils/db.py
+++ ziho/utils/db.py
@@ -1,4 +1,4 @@
-from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.exc import DatabaseError, SQLAlchemyError
 
 from ziho.core.exceptions import PersistenceError
 
@@ -7,4 +7,6 @@ def try_commit(session, msg):
     try:
         session.commit()
     except SQLAlchemyError:
+        raise PersistenceError(msg)
+    except DatabaseError:
         raise PersistenceError(msg)
