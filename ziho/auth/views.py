34ef3ba7540b466f628e15db5798d2bc1e494a49
Pratham <prathms007@gmail.com>
2024-06-27 04:37:13 +0530

    auth: Handle db errors.

--- ziho/auth/views.py
+++ ziho/auth/views.py
@@ -1,11 +1,17 @@
 from urllib.parse import urlparse
 
 from flask import flash, redirect, render_template, request, url_for
+from flask.views import MethodView
 from flask_login import current_user, login_user, logout_user
+from flask_migrate import current
+from sqlalchemy.exc import SQLAlchemyError
 
 from ziho.auth import bp
 from ziho.auth.forms import LoginForm, RegistrationForm
 from ziho.auth.handlers import create_user, get_user_by_username
+from ziho.core.exceptions import PersistenceError
+from ziho.errors.errors import ServerError
+from ziho.utils.helper import get_response
 
 
 @bp.route("/login", methods=["GET", "POST"])
@@ -14,10 +20,13 @@ def login():
         return redirect(url_for("home.home"))
     form = LoginForm()
     if form.validate_on_submit():
-        user = get_user_by_username(form.username.data)
-        if user is None or not user.check_password(form.password.data):
-            flash("Invalid username or password.", "danger")
-            return redirect(url_for("auth.login"))
+        try:
+            user = get_user_by_username(form.username.data)
+            if user is None or not user.check_password(form.password.data):
+                flash("Invalid username or password.", "danger")
+                return redirect(url_for("auth.login"))
+        except SQLAlchemyError as e:
+            raise ServerError(get_response(message=e._message))
         login_user(user, remember=form.remember_me.data)
         next = request.args.get("next")
         if not next or urlparse(next).netloc != "":
@@ -38,8 +47,12 @@ def register():
         return redirect(url_for("home.home"))
     form = RegistrationForm()
     if form.validate_on_submit():
-        create_user(form.username.data, form.email.data, form.password.data)
-        flash("Congratulations, you are now a registered user!", "success")
-        return redirect(url_for("auth.login"))
+        try:
+            create_user(form.username.data, form.email.data, form.password.data)
+            flash("Congratulations, you are now a registered user!", "success")
+            return redirect(url_for("auth.login"))
+        except PersistenceError as e:
+            flash("Could not register user.", "danger")
+        return render_template("auth/register.html", title="Register", form=form)
     else:
         return render_template("auth/register.html", title="Register", form=form)
