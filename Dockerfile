803fa99ff649b81e2b7cea72a7c43a1ea822da0c
Pratham <prathms007@gmail.com>
2024-07-05 07:07:49 +0530

    tools: Update requirements along with setup and run scripts.

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,18 @@
 FROM python:alpine3.17
 WORKDIR /app
 
-ENV DOCKER_ENV docker
 COPY requirements.txt .
 RUN apk update && apk add bash
 RUN apk add --update py-pip
+RUN pip install -r requirements.txt
+
+ARG LOCAL_ENV
+ARG DOCKER_ENV
+ENV LOCAL_ENV $LOCAL_ENV
+ENV DOCKER_ENV $DOCKER_ENV
 
 COPY . .
-EXPOSE 5000
 
+EXPOSE 5000
 RUN sh tools/setup
 CMD ["sh", "tools/run-ziho"]
--- config.py
+++ config.py
@@ -1,16 +1,22 @@
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv()
+
+
+DATABASE = "sqlite:///" + os.path.join(basedir, "app.db")
+if os.environ.get("DATABASE") == "mysql":
+    pass
 
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "hardcoded-string"
 
     # sqlalchemy
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        "DATABASE_URL"
-    ) or "sqlite:///" + os.path.join(basedir, "app.db")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or DATABASE
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = Path("ziho_uploads")
     UPLOAD_PATH = Path(basedir) / "ziho" / UPLOAD_FOLDER
